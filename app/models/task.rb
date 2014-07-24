5c1f2d44e278bf4c6302716a4d40161f743100e6
nomadop <nomadop@gmail.com>
2014-07-24 15:16:01 +0800

    match options

--- app/models/task.rb
+++ app/models/task.rb
@@ -31,9 +31,15 @@ class Task < ActiveRecord::Base
 		end
 		cname = options[:cname]
 		ccode = options[:ccode]
+		match_options = options[:match_options].keys.inject({}) do |result, key|
+			result[key.to_sym] = options[:match_options][key]
+			result
+		end
+		match_options ||= {}
+		simi_log(match_options, true)
 		Hotel.update_or_create_hotels_from_asiatravel_by_country_code(ccode)
 		Hotel.update_or_create_hotels_by_country_name_from_tripadvisor(cname, true, self)
-		Hotel.match_hotels_between_tripadvisor_and_asiatravel_by_country(cname, logger: self)
+		Hotel.match_hotels_between_tripadvisor_and_asiatravel_by_country(cname, match_options.merge(logger: self))
 	rescue Exception => e
 		error_log(level: :error) do |file|
 			file.puts "#{e.inspect}:"
